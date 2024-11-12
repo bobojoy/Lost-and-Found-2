a5dde660f604b6f323f0cc9aa7212218516584f3
junne.wanja@student.moringaschool.com <junne.wanja@student.moringaschool.com>
2024-11-12 14:31:05 +0300

    "Added imports for various components in Routes.js"

---
+++ src/Components/Routes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import reportWebVitals from './reportWebVitals';
+import Home from './Components/Home';
+import Lost Items  from './Components/Lost Items';
+import Found Items  from './Components/Found Items ';
+import Rewards from './Components/Rewards';
+import SignIn from './Components/SignIn';
+// import Signup from './Components/Signup';
+//import Login from './Components/Login';
