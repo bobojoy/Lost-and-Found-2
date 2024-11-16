e0020945c6a1298f4f777832ec79488d65bcae6a
Joy Wambui <joy.wambui@student.moringaschool.com>
2024-11-16 17:26:16 +0300

    Created a common  Landing Page

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-import LogOut from './Components/logout';
-import SuccessfulLogout from './Components/successfulLogin';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import "./App.css";
 
+// // Import the page components
+// import AdminPage from "./AdminPage";
+// import MemberPage from "./MemberPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <Router>
+      <div className="App">
+        <h1>Welcome to Our Website!</h1>
+        <p className="welcome-message">
+          We're glad to have you here. Please click below if you are a member or
+          an admin.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        </header>
-      
-      <BrowserRouter>
+
+        <div className="button-container">
+          <Link to="/admin">
+            <button className="admin-button">I am an Admin</button>
+          </Link>
+          <Link to="/member">
+            <button className="member-button">I am a Member</button>
+          </Link>
+        </div>
+      </div>
+{/* 
       <Routes>
-        <Route path="/" element={<LogOut />} />
-        <Route path="/successfullogout" element={<SuccessfulLogout />} />
-      </Routes>
-    </BrowserRouter>
-    </div>
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/member" element={<MemberPage />} />
+      </Routes> */}
+    </Router>
   );
 }
 
