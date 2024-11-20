4d8d81b759cd06b999d7702221ed151ef868d53b
allan <4lwachira@gmail.com>
2024-11-20 13:36:09 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Homepage from "./Homepage";
-import User from "./User/Components"; // Adjust if you have specific subcomponents
-import Admin from "./Admin/Components"; // Adjust if you have specific subcomponents
-
+import User from "./User/Components"; 
+import Admin from "./Admin/Components"; 
 const App = () => {
   return (
     <Router>
